e20a045fe63bf8f20c24dc6d7400b388dafcb7fe
Ricardo <ricardomon85@gmail.com>
2022-12-15 19:02:37 -0600

    Creacion de todos los metodo y codigos 200 y codigos 400 con sus excepciones

--- src/main/java/edu/tienda/core/controllers/ClienteRestController.java
+++ src/main/java/edu/tienda/core/controllers/ClienteRestController.java
@@ -1,13 +1,20 @@
 package edu.tienda.core.controllers;
 
 import edu.tienda.core.domain.Cliente;
+import edu.tienda.core.exceptions.BadRequestException;
+import edu.tienda.core.exceptions.ResourceNotFoundException;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteRestController {
 
     private List<Cliente> clientes = new ArrayList<>(Arrays.asList(
@@ -16,26 +23,61 @@ public class ClienteRestController {
             new Cliente("col", "1234", "Collins")
     ));
 
-    @GetMapping("/clientes")
-    public List<Cliente> getClientes(){
-        return clientes;
+    @GetMapping
+    public ResponseEntity<?> getClientes(){
+
+        return ResponseEntity.ok(clientes);
     }
 
-    @GetMapping("/clientes/{userName}")
-    public Cliente getCliente(@PathVariable String userName){
+    @GetMapping("/{userName}")
+    public ResponseEntity<?> getCliente(@PathVariable String userName){
+
+        if (userName.length() != 3){
+            throw new BadRequestException("EL parametro del nombre debe contener 2 carateres");
+        }
+
         for (Cliente cli: clientes){
             if (cli.getUsername().equalsIgnoreCase(userName)){
-                return cli;
+                return  ResponseEntity.ok(cli);
             }
         }
 
-        return null;
+        throw new ResourceNotFoundException("CLiente no encontrado");
     }
 
-    @PostMapping("/clientes")
-    public Cliente altaCliente(@RequestBody Cliente cliente){
+    @PostMapping
+    public ResponseEntity<?> altaCliente(@RequestBody Cliente cliente){
         clientes.add(cliente);
-        return cliente;
+
+        //Obtenemos la url del servicio al nuevo cliente que se creo
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{userName}")
+                .buildAndExpand(cliente.getUsername())
+                .toUri();
+
+        return ResponseEntity.created(location).body(cliente);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> modificacionCliente(@RequestBody Cliente cliente){
+        Cliente clienteEcontrado = clientes.stream().
+                filter(cli -> cli.getUsername().equalsIgnoreCase(cliente.getUsername())).
+                findFirst().orElseThrow();
+
+        clienteEcontrado.setPassword(cliente.getPassword());
+        clienteEcontrado.setNombre(cliente.getNombre());
+        return ResponseEntity.ok(cliente);
+    }
+
+    @DeleteMapping("/{userName}")
+    public ResponseEntity deleteCLiente(@PathVariable String userName){
+        Cliente clienteEncontrado = clientes.stream().
+                filter(cli -> cli.getUsername().equalsIgnoreCase(userName)).
+                findFirst().orElseThrow();
+
+        clientes.remove(clienteEncontrado);
+        return ResponseEntity.noContent().build();
     }
 
 }
